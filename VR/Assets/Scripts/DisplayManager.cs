93d772dcbb510038bb0e374356ce9ac6d7eca930
5im's <seotoons@gmail.com>
2025-09-30 14:55:33 +0200

    beats

---
+++ VR/Assets/Scripts/DisplayManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DisplayManager : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+            }
+        }
+    }
+}
